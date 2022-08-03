d9d64dd4216a756ee5194980b474fe5e7faed436
ahmad1091 <ahmadtayeb91@gmail.com>
2022-08-03 12:38:52 +0300

    add the secerts users

---
+++ secret.py
@@ -0,0 +1,32 @@
+#ANEES APP KEYS
+
+user1 = {
+    'CONSUMER_KEY':'',
+    'CONSUMER_SECRET':'',
+    'ACCESS_TOKEN':'',
+    'ACCESS_SECRET':'',
+    'FILE_NAME':'',
+    'LOG_FILE':'',
+    'USER_NAME':''
+}
+
+
+user2 = {
+    'CONSUMER_KEY':'',
+    'CONSUMER_SECRET':'',
+    'ACCESS_TOKEN':'',
+    'ACCESS_SECRET':'',
+    'FILE_NAME':'test_user_data.txt',
+    'LOG_FILE':'test_log_file.txt'
+}
+user3 = {
+    'CONSUMER_KEY':'',
+    'CONSUMER_SECRET':'',
+    'ACCESS_TOKEN':'',
+    'ACCESS_SECRET':'',
+    'FILE_NAME':'test_user_data.txt',
+    'LOG_FILE':'test_log_file.txt'
+}
+
+
+accounts = [ammar]
